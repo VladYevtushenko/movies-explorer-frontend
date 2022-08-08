9b6a7dd31fc53f3e7a5cb748efe411ab34463d69
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-08-08 20:03:05 +0300

    develope footer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,7 @@ import { Promo } from './Promo/Promo'
 import { AboutProject } from './AboutProject/AboutProject'
 import { Techs } from './Techs/Techs';
 import { AboutMe } from './AboutMe/AboutMe';
+import { Footer } from '../Footer/Footer';
 
 export const Main = () => {
     return (
@@ -16,6 +17,7 @@ export const Main = () => {
                 <Techs />
                 <AboutMe />
             </main>
+            <Footer />
         </>
     );
 };
