04d5e7d139824fc9cb68a1ef8a0ee7b9836e4eeb
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-08-09 22:51:29 +0300

    complete header functionality

--- src/components/Header/Components/Navigation/Components/NavigationLoggedIn/NavigationLoggedIn.js
+++ src/components/Header/Components/Navigation/Components/NavigationLoggedIn/NavigationLoggedIn.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './NavigationLoggedIn.css';
 import accIcon from '../../../../../../images/account-icon.svg';
+import { BurgerMenu } from './BurgerMenu/BurgerMenu';
 
 export const NavigationLoggedIn = () => {
+    const [opened, setOpened] = useState(true);
+
+    const onOpenMenu = () => {
+        setOpened(false);
+    };
+
+    const onCloseMenu = () => {
+        setOpened(true);
+    };
+
     return (
         <>
+        {opened 
+            ? (<div className='burger-btn' onClick={onOpenMenu}></div>)
+            : (<BurgerMenu onCloseMenu={onCloseMenu} />)
+        }
         <nav className='navi-logged__menu-bar'>
-            <div className='navi-logged__movies'>
-                <NavLink to='/movies' className='navi-logged__item'>
+            <div className='navi-logged__links'>
+                <NavLink 
+                    to='/movies' 
+                    className={({ isActive }) => 
+                        isActive 
+                        ? 'navi-logged__item active'
+                        : 'navi-logged__item' }
+                >
                     Фильмы
                 </NavLink>
-                <NavLink to='/saved-movies' className='navi-logged__item'>
+                <NavLink 
+                    to='/saved-movies' 
+                    className={({ isActive }) => 
+                        isActive 
+                        ? 'navi-logged__item active'
+                        : 'navi-logged__item' }
+                >
                     Сохраненные фильмы
                 </NavLink>
             </div>
             <div className='navi-logged__account'>
-                <NavLink to='/profile' className='navi-logged__account_link'>
+                <NavLink 
+                    to='/profile' className='navi-logged__account_link'>
                     Аккаунт
                 </NavLink>
                 <div className='navi-logged__account_icon-box'>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,13 @@ import './Header.css';
 import { Navigation } from './Components/Navigation/Navigation';
 import logo from '../../images/logo.svg';
 
-export const Header = () => {
+export const Header = ({ loggedIn }) => {
     return (
         <header className='header header_main'>
             <Link to='/'>
                 <img className='header__logo' src={logo} alt='logo' />
             </Link>
-            <Navigation />
+            <Navigation loggedIn={loggedIn}/>
         </header>
     );
 };
