2c0896ac0f9e9f96b105d4054ee03bd4288619e2
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-08-11 00:57:00 +0300

    developing searchform fix checkbox

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,31 +1,35 @@
 import './SearchForm.css';
+import lupa from '../../images/lupa-icon.svg' 
 
 export const SearchForm = () => {
-    <section className='search-form'>
-        <form className='search-form__box'>
-            <div className='search-form__field'>
-                <input 
-                    className='search-form__input'
-                    type='text'
-                    placeholder={`Фильм`}
-                    name='film'
-                    required
-                />
-                <button 
-                    className='search-form__btn'
-                    type='button'
-                ></button>
-            </div>
-            <label className='tumbler__label'>
-                <input 
-                    className='checkbox'
-                    name='short-film'
-                    type='checkbox'
-                />
-                <span className='checkbox__icon'></span>
-                Короткометражки
-            </label>
-        </form>
-        {/* <span className='search-form__error'>{messageError}</span> */}
-    </section>
+    return (
+        <section className='search-form'>
+            <form className='search-form__box'>
+                <div className='search-form__field'>
+                    <img className='lupa' src={lupa} alt='lupa icon' />
+                    <input 
+                        className='search-form__input'
+                        type='text'
+                        placeholder={`Фильм`}
+                        name='film'
+                        required
+                    />
+                    <button 
+                        className='search-form__btn'
+                        type='button'
+                    ></button>
+                </div>
+                <label className='tumbler__label'>
+                    <input 
+                        className='checkbox'
+                        name='short-film'
+                        type='checkbox'
+                    />
+                    <span className='checkbox__icon'></span>
+                    Короткометражки
+                </label>
+            </form>
+            {/* <span className='search-form__error'>{messageError}</span> */}
+        </section>
+    );
 };
