728e4d1aa24f55a7c44668a7ea0d8b8dd4075182
Vladislav Yevtushenko <vladislavyevtushenko@yandex.ru>
2022-08-16 15:18:01 +0300

    develop 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,10 +4,10 @@ import './NotFound.css';
 
 export const NotFound = () => {
     return (
-        <section className='not-found'>
+        <div className='not-found'>
             <h2 className='not-found__title'>404</h2>
             <p className='not-found__subtitle'>Страница не найдена</p>
-            <Link className='not-found__return'>Назад</Link>
-        </section>
+            <Link to='/' className='not-found__return'>Назад</Link>
+        </div>
     );
 };
